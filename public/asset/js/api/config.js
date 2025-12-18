81ab8300e8edbc55cad843f742a3a07edcc0cae9
jej3zin <jeanholiveria2003@gmail.com>
2025-12-18 14:21:01 -0300

    Ajustes

--- public/asset/js/api/config.js
+++ public/asset/js/api/config.js
@@ -2,6 +2,6 @@
 window.API_BASE =
   location.hostname === 'localhost'
     ? 'http://localhost:3001'
-    : 'https://api.mafiadopix.com';
+    : 'postgresql://server_ox4h_user:MdNVRF2MobaqRIpwpx3biGYuytmRX9pf@dpg-d5202e3e5dus73aj8tu0-a/server_ox4h';
 
 const API_BASE = window.API_URL || window.API_BASE;
